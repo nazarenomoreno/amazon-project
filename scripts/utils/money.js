9e7e7d699187939cb1f6380bd09c17e05b1ce9c4
naza <102782284+nazarenomoreno@users.noreply.github.com>
2024-10-28 18:56:51 -0300

    crear test automaticos para formatCurrency

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,5 +1,5 @@
 
-export function formatCurrency(priceCents){
+export function formatCurrency(priceCents){             // transforma los centavos a dolares
   return (Math.round(priceCents)/100).toFixed(2);
 
 }
