17ca7be007acdba8a497f7373ac683dbebcd4ffb
nazareno <nazamoreno16@gmail.com>
2025-01-30 17:04:19 -0300

    cambiar carrito inicial

--- data/cart.js
+++ data/cart.js
@@ -10,18 +10,8 @@ export function loadFromStorage(){        //funcion para cargar desde el localSt
   //al recargar el localStorage se cargara el carrito modificado o si esta vacio, uno nuevo
   
   if(!cart){                             //si el carrito es falsy (null), es decir, esta vacio:
-    cart= [{                                                 //se llena (no se inicializa, se llena)
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId:'1'
-  
-    },
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId:'2'
-    }];
-  }
+    cart= [];                             //se llena, no se inicializa
+   }
 
  
 };
